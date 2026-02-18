6c41cb6054542f44b381975b75abd15ac0d6e9c9
Mazen Kassem <kassemmazen@gmail.com>
2026-02-18 00:47:07 -0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || "Iftar time (Maghrib)";
+  const options = {
+    body: data.body || "It’s time for Iftar.",
+    icon: "/icons/icon-192.png",
+    badge: "/icons/icon-192.png",
+    data: { url: data.url || "/" }
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const url = event.notification.data?.url || "/";
+  event.waitUntil(clients.openWindow(url));
+});
